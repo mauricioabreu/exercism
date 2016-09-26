c8fa107842c82f50a6ee5d48ccd1508c0251ec9b
Mauricio Antunes <mauricio.antunes@azion.com>
2016-09-26 09:43:56 -0300

    refactor: Remove unnecessary array cast
    Range is already an Enumerable so it does not need to be transformed
    into an array to use operations on it, such as map.

--- crystal/pangram/pangram.cr
+++ crystal/pangram/pangram.cr
@@ -1,5 +1,5 @@
 module Pangram
   def self.pangram?(statement)
-    ('a'..'z').to_a.map { |l| statement.downcase.includes?(l) }.all?
+    ('a'..'z').map { |l| statement.downcase.includes?(l) }.all?
   end
 end
