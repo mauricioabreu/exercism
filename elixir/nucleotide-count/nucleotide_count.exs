b634de4cc9167d749d8e714b32263cd448e2f99c
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2016-10-12 13:12:27 -0300

    feat: Add Elixir nucleotide exercise

---
+++ elixir/nucleotide-count/nucleotide_count.exs
@@ -0,0 +1,33 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a NucleotideCount strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) do
+    Enum.count(strand, fn(letter) -> letter == nucleotide end)
+  end
+
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram([char]) :: map
+  def histogram(strand) do
+    Enum.map(@nucleotides, &{&1, count(strand, &1)}) |> Enum.into(%{})
+  end
+end
