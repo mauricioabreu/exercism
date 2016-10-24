68c7f5c1c21aa1f5d7d79d082abc1ed8ed18a5a5
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2016-10-23 22:53:37 -0200

    feat: Add Elixir Acronym exercise

---
+++ elixir/acronym/acronym.exs
@@ -0,0 +1,13 @@
+defmodule Acronym do
+  @doc """
+  Generate an acronym from a string.
+  "This is a string" => "TIAS"
+  """
+  @spec abbreviate(String.t()) :: String.t()
+  def abbreviate(string) do
+    Regex.scan(~r/[A-Z]+[a-z]*|[a-z]+/, string) |>
+    List.flatten |>
+    Enum.map(fn(word) -> String.upcase(String.first(word)) end) |>
+    Enum.join
+  end
+end
