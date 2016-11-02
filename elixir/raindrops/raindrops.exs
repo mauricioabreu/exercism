8b710dd8105ef2fa7160552033381b22b4a3ef2e
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2016-11-02 13:00:05 -0200

    refactor: Improve algorithm used on Raindrops

--- elixir/raindrops/raindrops.exs
+++ elixir/raindrops/raindrops.exs
@@ -8,19 +8,22 @@ defmodule Raindrops do
   - If the number does not contain 3, 5, or 7 as a prime factor,
     just pass the number's digits straight through.
   """
+
+  @pwords %{3 => "Pling", 5 => "Plang", 7 => "Plong"}
+
   @spec convert(pos_integer) :: String.t
   def convert(number) do
-    filtered = Enum.map([3, 5, 7], fn(n) -> rem(number, n) == 0 end)
-    Enum.zip(["Pling", "Plang", "Plong"], filtered) |>
-    Enum.filter_map(fn(x) -> elem(x, 1) == true end, &(elem(&1, 0))) |>
-    output(number)
+    Map.keys(@pwords)
+    |> Enum.map(&(psound(number, &1)))
+    |> Enum.join
+    |> output(number)
   end
 
-  defp output(primes, number) do
-    if Enum.empty?(primes) do
-      Integer.to_string(number)
-    else
-      Enum.join(primes)
-    end
+  defp psound(number, divisible) do
+    if rem(number, divisible) == 0, do: @pwords[divisible]
+  end
+
+  defp output(pwords, number) do
+    if pwords == "", do: to_string(number), else: pwords
   end
 end
