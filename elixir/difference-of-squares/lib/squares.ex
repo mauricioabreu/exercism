cba5135faaa9cac36da7f911384265c4abfbe3d8
Maurício Antunes <mauricio.abreua@corp.globo.com>
2021-04-23 20:06:19 -0300

    Apply some math to squares difference

--- elixir/difference-of-squares/lib/squares.ex
+++ elixir/difference-of-squares/lib/squares.ex
@@ -7,17 +7,16 @@ defmodule Squares do
   Calculate sum of squares from 1 to a given end number.
   """
   @spec sum_of_squares(pos_integer) :: pos_integer
-  def sum_of_squares(number) do
-    Enum.map(1..number, fn n -> n * n end)
-    |> Enum.sum
+  def sum_of_squares(n) do
+    n * (n + 1) * (2 * n + 1) / 6
   end
 
   @doc """
   Calculate square of sum from 1 to a given end number.
   """
   @spec square_of_sum(pos_integer) :: pos_integer
-  def square_of_sum(number) do
-    a = Enum.sum(1..number)
+  def square_of_sum(n) do
+    a = n * (n + 1) / 2
     a * a
   end
 
@@ -25,7 +24,7 @@ defmodule Squares do
   Calculate difference between sum of squares and square of sum from 1 to a given end number.
   """
   @spec difference(pos_integer) :: pos_integer
-  def difference(number) do
-    square_of_sum(number) - sum_of_squares(number)
+  def difference(n) do
+    square_of_sum(n) - sum_of_squares(n)
   end
 end
