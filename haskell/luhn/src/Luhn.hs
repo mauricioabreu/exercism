e5f3e71ceab415b05e2dfebdedf2d345f9ba1876
Maurício Antunes <mauricio.abreua@gmail.com>
2022-01-02 22:08:25 -0300

    Refactor Luhn

--- haskell/luhn/src/Luhn.hs
+++ haskell/luhn/src/Luhn.hs
@@ -1,22 +1,12 @@
 module Luhn (isValid) where
-import Data.Char (isSpace)
-import Text.Read (readMaybe)
+import Data.Char (isDigit, isSpace, digitToInt)
 
 isValid :: String -> Bool
 isValid s
     | length cInput <= 1 = False
-    | not $ isNumber cInput = False
-    | otherwise = validate $ toDigits cInput
-    where cInput = trimWhitespace s
-
-trimWhitespace :: String -> String
-trimWhitespace = filter (not . isSpace)
-
-isNumber :: String -> Bool
-isNumber n = case n' of
-    Nothing -> False
-    Just _ -> True
-    where n' = readMaybe n :: Maybe Int
+    | isValidFormat s = False
+    | otherwise = checksum cInput
+    where cInput = toDigits s
 
 double :: Int -> Int
 double n
@@ -25,14 +15,16 @@ double n
     where dn = n * 2
 
 toDigits :: String -> [Int]
-toDigits n = reverse n'
-    where n' = [read [x] :: Int | x <- n]
+toDigits = map digitToInt . reverse . filter isDigit
 
 applyToEverySnd :: (a -> a) -> [a] -> [a]
 applyToEverySnd _ [] = []
 applyToEverySnd _ [x] = [x]
 applyToEverySnd f (x:y:xs) = x : f y : applyToEverySnd f xs
 
-validate :: [Int] -> Bool
-validate xs = s `rem` 10 == 0
+checksum :: [Int] -> Bool
+checksum xs = s `rem` 10 == 0
     where s = sum $ applyToEverySnd double xs
+
+isValidFormat :: String -> Bool
+isValidFormat = not . all (\x -> isSpace x || isDigit x)
