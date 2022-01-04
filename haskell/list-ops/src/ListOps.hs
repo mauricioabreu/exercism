69cab09a574219413c46cc561abc1583799bc0bf
Maurício Antunes <mauricio.abreua@gmail.com>
2022-01-04 09:29:12 -0300

    Reuse foldr and foldl'

--- haskell/list-ops/src/ListOps.hs
+++ haskell/list-ops/src/ListOps.hs
@@ -22,31 +22,21 @@ foldr :: (a -> b -> b) -> b -> [a] -> b
 foldr _ z [] = z
 foldr f z (x:xs) = f x (foldr f z xs)
 
-
 length :: [a] -> Int
 length = foldr (\_ -> (+) 1) 0
 
 reverse :: [a] -> [a]
-reverse xs = reverse' xs []
-
-reverse' :: [a] -> [a] -> [a]
-reverse' xs acc = foldl' (flip (:)) acc xs
+reverse = foldl' (flip (:)) []
 
 map :: (a -> b) -> [a] -> [b]
-map _ [] = []
-map f (x:xs) = f x : map f xs
+map f = foldr (\x xs -> f x : xs) []
 
 filter :: (a -> Bool) -> [a] -> [a]
 filter _ [] = []
 filter p (x:xs) = if p x then x : filter p xs else filter p xs
 
 (++) :: [a] -> [a] -> [a]
-[] ++ [] = []
-xs ++ [] = xs
-[] ++ ys = ys
-(x:xs) ++ ys = x:xs ++ ys
+xs ++ ys = foldr (:) ys xs
 
 concat :: [[a]] -> [a]
-concat [] = []
-concat [xs] = xs
-concat (xs:xss) = xs ++ concat xss
+concat = foldr (++) []
