a577b27d4ddab3b25bbb2fa854405dd093bcea28
Maurício Antunes <mauricio.abreua@gmail.com>
2022-01-12 09:01:57 -0300

    Refactor collatz using succ and func application

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,13 +1,8 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz s
-    | s <= 0 = Nothing
-    | otherwise = Just (collatz' s 0)
-
-
-collatz' :: Integer -> Integer -> Integer
-collatz' n steps
-    | n == 1 = steps
-    | otherwise = collatz' n' (steps + 1)
-    where n' = if even n then n `div` 2 else 3 * n + 1
+collatz n
+    | n <= 0 = Nothing
+    | n == 1 = Just 0
+    | even n = succ <$> collatz (n `div` 2)
+    | otherwise = succ <$> collatz (3 * n + 1)
